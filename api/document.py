861f0fdd2bd047b95f173ced08ed34c86450905e
mizanin16_notebook <b_dimon2009@mail.ru>
2025-05-02 18:40:57 +0300

    ResponseTooLargeError fixed

--- api/document.py
+++ api/document.py
@@ -5,5 +5,9 @@ from models.schemas import FileResult
 router = APIRouter()
 
 @router.get("/full-document", response_model=FileResult)
-async def full_document(file_name: str = Query(...), collection: str = Query(...)):
-    return await FullDocumentService.load_full_document(collection, file_name)
+async def full_document(
+    file_name: str = Query(...),
+    collection: str = Query(...),
+    truncate: bool = Query(True)
+):
+    return await FullDocumentService.load_full_document(collection, file_name, truncate)
