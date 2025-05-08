7a4de716dd0feea7246b92dbec9a5ba54c57621f
mizanin16_notebook <b_dimon2009@mail.ru>
2025-05-08 11:05:30 +0300

    add test for response openai

--- api/test_response.py
+++ api/test_response.py
@@ -9,5 +9,5 @@ async def test_response_size(size: int = Query(..., ge=1, le=500000)):
     Возвращает JSON-объект с полем `payload`, содержащим строку заданной длины.
     Используется для тестирования ограничений на размер ответа.
     """
-    content = "х" * size
+    content = "купец" * (size // 5)
     return JSONResponse(content={"length": len(content), "payload": content})
